fec338eb68e40362f96827f31c8cf03a87f6d95e
waleed-05 <waleedfen@gmail.com>
2026-04-19 12:55:27 +0500

    Refactor port scanner for improved readability and structure

--- port scanner.py
+++ port scanner.py
@@ -1,39 +1,37 @@
 import sys
 import socket
-import threading
 import time
+import threading
 
-
-usage=("python3 paractise.py IP START_PORT END_PORT")
+usage="python3 paractise.py ip start_port end_port"
 print("-"*70)
-print("simple port scanner")
+print("python simple port scanner")
 print("-"*70)
 
-if len(sys.argv)!=4:
-    print(usage)
-    sys.exit()
-start_time= time.time()
+if len(sys.argv) != 4:
+    print(usage)
+    sys.exit()
+start_time=time.time()
 try:
-    target=socket.gethostbyname(sys.argv[1])
+   target= socket.gethostbyname(sys.argv[1])
 except socket.gaierror:
-    print("Ip resolution failed")
-    sys.exit()
+    print("name resolution error")
+    sys.exit()
 
 start_port=int(sys.argv[2])
 end_port=int(sys.argv[3])
+def port_scanner(port):
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    conn=s.connect_ex((target,port,))
+    s.settimeout(1)
+    if (not conn):
+        print("port {} is open".format(port))
+        s.close()
 
-def port_scan(port):
-    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    conn= s.connect_ex((target,port,))
-    s.settimeout(1)
-    if (not conn):
-        print("port number {} is open".format(port))
-    s.close()
+for port in range(start_port, end_port +1): # +1 to include last port.
+    thread= threading.Thread(target=port_scanner,args=(port,))
+    thread.start()    
 
-for port in range(start_port, end_port +1):
-    thread=threading.Thread(target=port_scan,args=(port,))
-    thread.start()
 end_time=time.time()
 total_time=end_time-start_time
-
-print("total time",total_time)
+print("results in", total_time)
